d0cf5d89f87b9f027b7040e35f3ef7f398a6e358
huanghaoxiang <huanghaoxiang@dobest.com>
2019-07-18 16:11:56 +0800

    删除不必要的类数据和linux下编译警告

--- mobaserver/moba_server_core/src/z/net/u_connection.h
+++ mobaserver/moba_server_core/src/z/net/u_connection.h
@@ -27,11 +27,6 @@ public:
     std::string user_id() const { return user_id_; }
     void set_user_id(const std::string &_user_id) {user_id_ = _user_id;}
     
-    void set_open_id(int64 _open_id) {open_id_ = _open_id;}
-    int64 open_id() const {return open_id_;}
-    void set_sid(int32 _sid) {sid_ = _sid;}
-    int32 sid() const {return sid_;}
-    
     bool IsForwarding() const
     {
         return status_ > LoginStatus_PLAYER_LOGIN && status_ < LoginStatus_CLOSING;
@@ -46,14 +41,12 @@ protected:
 
     int32 session_id_;
     std::string user_id_;
-    int64 open_id_;
-    int32 sid_;
 
     IUMsgHandler* msg_handler_;
 
-    LoginStatus status_;
-
 	kcp_conv_t kcp_conv_t_;
+
+    LoginStatus status_;
 
     DISALLOW_COPY_AND_ASSIGN(UConnection);
 };
