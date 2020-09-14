5fb3cda4dc6d22e3b0ea53f87e20f857ece0b261
huanghaoxiang <huanghaoxiang@dobest.com>
2020-09-14 13:36:33 +0800

    编译错误 注释错误

--- mobaserver/moba_server_core/src/z/net/http_server_session.h
+++ mobaserver/moba_server_core/src/z/net/http_server_session.h
@@ -22,7 +22,7 @@ namespace z {
 			};
 
 			// Returns a not found response
-			auto const not_found =
+			/*auto const not_found =
 				[&req](boost::beast::string_view target)
 			{
 				boost::beast::http::response<boost::beast::http::string_body> res{ boost::beast::http::status::not_found, req.version() };
@@ -32,10 +32,10 @@ namespace z {
 				res.body() = "The resource '" + std::string(target) + "' was not found.";
 				res.prepare_payload();
 				return res;
-			};
+			};*/
 
 			// Returns a server error response
-			/*auto const server_error =
+			auto const server_error =
 				[&req](boost::beast::string_view what)
 			{
 				boost::beast::http::response<boost::beast::http::string_body> res{ boost::beast::http::status::internal_server_error, req.version() };
@@ -45,7 +45,7 @@ namespace z {
 				res.body() = "An error occurred: '" + std::string(what) + "'";
 				res.prepare_payload();
 				return res;
-			};*/
+			};
 
 			// Make sure we can handle the method
 			if (req.method() != boost::beast::http::verb::post &&
